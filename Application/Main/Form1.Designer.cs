8e4c82087b66f8e19525e95acab1cbf83a552194
Nguyen Tien Dat <maxdat2003@gmail.com>
2024-07-18 20:38:03 +0700

    test_connect-db

--- Application/Main/Form1.Designer.cs
+++ Application/Main/Form1.Designer.cs
@@ -29,13 +29,25 @@ namespace Main
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Test Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(773, 428);
+            this.ClientSize = new System.Drawing.Size(594, 310);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +55,8 @@ namespace Main
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Application/Main/Form1.cs
+++ Application/Main/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,30 @@ namespace Main
 {
     public partial class Form1 : Form
     {
+        private SqlConnection connection;
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void InitializeMySqlConnection()
+        {
+            string connectionString = @"Data Source=DESKTOP-8I1VIDD\SQLEXPRESS;Initial Catalog=Module_DN;Persist Security Info=True;User ID=sa;Password =123";
+            connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                MessageBox.Show("Kết nối thành công!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kết nối thất bại" + ex.Message);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InitializeMySqlConnection();
         }
     }
 }
